7864873b77995e62b2fe7d794f91912f6d026010
Andre <andrehenssler@sou.faccat.br>
2025-06-21 11:13:37 -0300

    Atualiza server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml'); // Caminho para o arquivo swagger.yaml
 const app = express(); 
 const userRoutes = require('./src/routes/user.routes'); // ou caminho relativo correto
 
 app.use(express.json()); // Middleware para analisar o corpo da requisição como JSON
+
+
+app.use(express.static('public'));
 //       // Retornar o token e os dados do usuário (sem a senha)
 app.use('/api/usuarios', userRoutes);
-
 //       return res.status(200).json({ token, usuario: { ...usuario, senha: undefined } });
 app.listen(3000, () => console.log('Servidor rodando na porta http://localhost:3000')); 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/user.routes');
-// const obraRoutes = require('./routes/obra.routes');
+const obraRoutes = require('./routes/obra.routes');
 const authMiddleware = require('./middlewares/auth.middleware');
 
 require('dotenv').config();
@@ -10,6 +10,6 @@ require('./config/db'); // conexão com o banco
 app.use(express.json());
 
 app.use('/api/usuarios', userRoutes);
-// app.use('/api/obras',authMiddleware, obraRoutes);
+app.use('/api/obras',authMiddleware, obraRoutes);
 
 module.exports = app;
