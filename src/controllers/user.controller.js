d42edfc90fbfafec672138aa401a6046e20900c6
Andre <andrehenssler@sou.faccat.br>
2025-06-28 16:44:07 -0300

    Atualiza User Controler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,18 @@
 const { Usuario } = require('../models');
 const bcrypt = require('bcrypt');
 
+// const Usuario = require('../models/usuario');
+
 exports.createUser = async (req, res) => {
-  const { nome, email, senha, tipo } = req.body;
-
-  if (!nome || !email || !senha) {
-    return res.status(400).json({ mensagem: 'Nome, email e senha são obrigatórios' });
+  try {
+    const user = await Usuario.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    // Sequelize Unique Constraint Error tem nome 'SequelizeUniqueConstraintError'
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      return res.status(400).json({ message: 'Erro: Email já cadastrado!' });
+    }
+    console.error('Erro ao criar usuário:', error);
+    res.status(500).json({ message: 'Erro interno no servidor.' });
   }
-
-  const hashed = await bcrypt.hash(senha, 10);
-  const user = await Usuario.create({ nome, email, senha: hashed, tipo });
-  res.status(201).json(user);
 };
